fd50d0a2f0df1f2ee95908deea2bad9adb8b6568
hoapham96 <89959754+hoapham96@users.noreply.github.com>
2022-04-14 14:54:17 -0500

    create dashboard

---
+++ client/src/components/Header/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Auth from '../../utils/auth';
+
+const Header = () => {
+  const logout = event => {
+    event.preventDefault();
+    Auth.logout();
+  };
+
+  return (
+    <header className="bg-secondary mb-4 py-2 flex-row align-center position-sticky">
+      <div className="container flex-row justify-space-between-lg justify-center align-center">
+        <Link to="/">
+          <h1>Pokémon Card</h1>
+        </Link>
+
+        <nav className="text-center">
+          {Auth.loggedIn() ? (
+            <>
+              <Link to="/profile">Me</Link>
+              <a href="/" onClick={logout}>
+                Logout
+              </a>
+            </>
+          ) : (
+            <>
+              <Link to="/shop">Shop</Link>
+              <Link to="/dashboard">Dashboard</Link>
+              <Link to="/login">Login</Link>
+              <Link to="/signup">Signup</Link>
+            </>
+          )}
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
