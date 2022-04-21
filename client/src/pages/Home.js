e93ebb4b1753bcc83d82527c0f9bf94d5e6af9d3
Xavyr Rodriguez <xavyrrodriguez@Xavyrs-Air.attlocal.net>
2022-04-21 13:58:40 -0500

    fixed image layout on homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -13,7 +13,7 @@ const Home = () => {
             </p>
         </div>
         <div className="image-fill ">
-          <img alt="pikachu" src="https://cdn2.myminifactory.com/assets/object-assets/5f0c5ee8d58d9/images/720X720-eevee2-2.jpg" />
+          <img className='eevee' alt="pikachu" src="https://cdn2.myminifactory.com/assets/object-assets/5f0c5ee8d58d9/images/720X720-eevee2-2.jpg" />
         </div>
       </div>
       
